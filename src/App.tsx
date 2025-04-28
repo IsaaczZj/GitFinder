5fdfb05c6442a17d919e2cbfa5a356f4cdfe6c47
IsaaczZj <joseisaac.isaac123@gmail.com>
2025-04-28 14:45:41 -0300

    configurando react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 
+import { Outlet } from 'react-router'
 import './App.css'
 function App() {
 
   return (
     <>
      <h1>GitFinder</h1>
+     <Outlet/>
     </>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.tsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
 
-createRoot(document.getElementById('root')!).render(
+import Home from "./Pages/Home.tsx";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
